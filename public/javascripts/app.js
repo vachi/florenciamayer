5c8380e5979be2dc8bc29efb543e523db2ff80a9
vachi <vache@dendritecorp.com>
2013-11-20 00:33:01 -0500

    new changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,18 +1,61 @@
 !function ($) {
     $(function(){
-        $('.navbar-nav a').click(function(){
+        $('.navbar a').click(function(){
             $('html, body').animate({
                 scrollTop: $( $.attr(this, 'href') ).offset().top
             }, 500);
-            $(".navbar-nav li").removeClass("active");
+            $(".navbar li").removeClass("active");
             $(this).parent("li").addClass("active");
             return false;
         });
     });
-
-    $('.carousel').carousel('pause');
-
+    
     $("img").unveil();
+
+    $(".tile > a").click( function(){
+        link = $(this).attr("href");
+        $(link).show().addClass("open");
+    });
+
+    $("button.closeTile").click( function(){        
+        $(".popUp").removeClass("open").hide();
+    });
+        
+
+    var resizer = function () {
+        var theWindow = $(window),
+            width = theWindow.width(),
+            height = theWindow.height();
+
+        $(".navbar").css({            
+            minHeight: height
+        });
+
+        $("article").css({            
+            maxWidth: width-180
+        });
+
+        $(".section").css({            
+            minHeight: height-100
+        });
+
+        $(".popUp").css({            
+            height: height,
+            width: width
+        });
+
+    };
+    resizer();      
+
+    $(window).resize(resizer);
+
+    $('.section').bind('inview', function (event, visible) {
+      if (visible == true) {
+        $(".section").fadeTo(400,1);        
+      } else {
+        $(this).fadeTo(400,0.5);
+      }
+    });
 
 }(window.jQuery)
 
--- public/javascripts/libs.js
+++ public/javascripts/libs.js
@@ -18,3 +18,65 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
 
 ;(function($){$.fn.unveil=function(threshold,callback){var $w=$(window),th=threshold||0,retina=window.devicePixelRatio>1,attrib=retina?"data-src-retina":"data-src",images=this,loaded;this.one("unveil",function(){var source=this.getAttribute(attrib);source=source||this.getAttribute("data-src");if(source){this.setAttribute("src",source);if(typeof callback==="function")callback.call(this);}});function unveil(){var inview=images.filter(function(){var $e=$(this),wt=$w.scrollTop(),wb=wt+$w.height(),et=$e.offset().top,eb=et+$e.height();return eb>=wt-th&&et<=wb+th;});loaded=inview.trigger("unveil");images=images.not(loaded);}$w.scroll(unveil);$w.resize(unveil);unveil();return this;};})(window.jQuery||window.Zepto);
 
+
+
+/**
+ * author Remy Sharp
+ * url http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+ */
+(function ($) {
+    function getViewportHeight() {
+        var height = window.innerHeight; // Safari, Opera
+        var mode = document.compatMode;
+
+        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
+            height = (mode == 'CSS1Compat') ?
+            document.documentElement.clientHeight : // Standards
+            document.body.clientHeight; // Quirks
+        }
+
+        return height;
+    }
+
+    $(window).scroll(function () {
+        var vpH = getViewportHeight(),
+            scrolltop = (document.documentElement.scrollTop ?
+                document.documentElement.scrollTop :
+                document.body.scrollTop),
+            elems = [];
+        
+        // naughty, but this is how it knows which elements to check for
+        $.each($.cache, function () {
+            if (this.events && this.events.inview) {
+                elems.push(this.handle.elem);
+            }
+        });
+
+        if (elems.length) {
+            $(elems).each(function () {
+                var $el = $(this),
+                    top = $el.offset().top,
+                    height = $el.height(),
+                    inview = $el.data('inview') || false;
+
+                if (scrolltop > (top + height) || scrolltop + vpH < top) {
+                    if (inview) {
+                        $el.data('inview', false);
+                        $el.trigger('inview', [ false ]);                        
+                    }
+                } else if (scrolltop < (top + height)) {
+                    if (!inview) {
+                        $el.data('inview', true);
+                        $el.trigger('inview', [ true ]);
+                    }
+                }
+            });
+        }
+    });
+    
+    // kick the event to pick up any elements already in view.
+    // note however, this only works if the plugin is included after the elements are bound to 'inview'
+    $(function () {
+        $(window).scroll();
+    });
+})(jQuery);
